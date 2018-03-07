c2af2794192f8c1bbd1926c63564e11e1dbb20c0
Olivier Argentieri <olivierargentieri@gmail.com>
2018-03-07 18:47:49 +0100

    ADD: BkModule

---
+++ mods/burgerKing.js
@@ -0,0 +1,28 @@
+const biguint = require('biguint-format');
+const crypto = require('crypto');
+module.exports = class BurgerKing{
+
+    static codeGenerate(message){
+        
+        // BK Month
+        var IndiceBK = ["BB", "LS", "JH", "PL", "BK", "WH", "FF", "BF", "CF", "CK", "CB", "VM"];
+
+       var messageToUpper = this.parse(message)
+        if (messageToUpper.includes("BK") || messageToUpper.includes("BURGERKING")) {
+            var date = new Date() 
+           var currentMonth = date.getMonth()
+            //general.send("Ton code KONAR : " + IndiceBK[currentMonth] + biguint(random(15), 'dec').substr(1, 5))
+            message.reply("Ton code : " + IndiceBK[currentMonth] + biguint(this.random(15), 'dec').substr(1, 5) + " :hamburger:")
+        }
+    };
+
+    static parse(message){
+        return message.content.toUpperCase().replace(" ", "");
+    };
+
+    // Random Function
+    static random (qty) {
+        return crypto.randomBytes(qty);
+    };
+}
+
