6d50e356c1af07f2684a1c7664d4083c02b9bda4
Craaftx <guilmain.dorian@gmail.com>
2018-03-28 15:00:36 +0200

    Add Tweeter Mod

--- mods/help.js
+++ mods/help.js
@@ -1,16 +1,56 @@
 /*
-  - Help Mod / Use with !help to show commands use in the bot
+*
+* - Help Mod 
+* / Use with !help to show commands use in the bot
+*
 */
-const Command = require("./command.js")
+
+// ---- Discord Core ----
+const Discord = require('discord.js');
+const Command = require("./command.js");
+
 module.exports = class Help extends Command{
+  static parse(message, modsConfig){
+    if(this.match(message)) {
+        this.action(message, modsConfig);
+        return true;
+    }
+    return false;     
+  };
+
   static match(message) {
       return message.content.startsWith('!help');
   };
   
-  static action (message) {
-      let args = message.content.split(' ');
-      args.shift();
-      message.delete();
-      message.reply('\n Liste des commandes disponibles : \n !help : Affiche l\'ensemble des commandes du bot \n');
+  static action(message, modsConfig) {
+      const embed = new Discord.RichEmbed();
+      const separator = "--------------------------------------"
+      
+      // Create message
+      embed.setAuthor("☕ Comment puis-je vous aider ?")
+      .setColor(3447003)
+      .setFooter("N'hésitez pas à me recontacter (!help)");
+
+      // Fill message
+      let modsField = "";
+      let baseField = this.help().corps;
+      for(let i = 0; i < modsConfig.length; i++){
+        if(modsConfig[i].mod === "base"){
+          baseField += modsConfig[i].corps;
+        } else {
+          modsField += modsConfig[i].corps;
+        }
+      }
+      embed.addField("__Commandes Basiques :__", baseField);
+      embed.addField("__Commandes des Mods :__", modsField);
+      // Send Message
+      message.channel.send({embed});
   };
+
+  static help() {
+    return {
+      mod: "base",
+      corps: " **Afficher l'ensemble des commandes du bot : ** \n ***!help***\n \n",
+    };
+  }
 }
---
+++ mods/twitter/userTweet.js
@@ -0,0 +1,65 @@
+/*
+*
+* -  Mod userTweet 
+* / Use with !usertweet {name} to get new tweet from a user
+*
+*/
+// Config
+const config = require('../../config.json');
+
+// Twitter
+const Twitter = require('twitter');
+const twitterApi = new Twitter(config.twitter_credentials);
+
+// Command Base
+const Command = require("../command.js");
+
+module.exports = class UserTweet extends Command{
+  static match(message) {
+      return message.content.startsWith('!usertweet');
+  };
+  
+  static action(message) {
+      let args = message.content.split(' ');
+      if(args[1]) {
+        if(args[2] > 10){
+          args[2] = 10;
+        } else {
+          if(!args[2]) { args[2] = 1; }
+        }
+        let tweetParams = {
+          q: '@' + args[1],
+          count: args[2],
+          result_type: 'recent',
+          lang: 'en',
+          exclude_replies: 'true'
+        };
+        console.log(tweetParams);
+        // Initiate your search using the above paramaters
+        twitterApi.get('search/tweets', tweetParams, function(err, data, response) {
+          // If there is no error, proceed
+          if(!err){
+            message.reply(':white_check_mark: Récupération automatiques de `' + args[2] + '` tweets de **' + args[1] + '**');
+            // Loop through the returned tweets
+            for(let i = 0; i < data.statuses.length; i++){
+              // Get the tweet Id from the returned data
+              message.reply(`https://twitter.com/${args[1]}/status/${data.statuses[i].id_str}`);
+            }
+          } else {
+            message.reply(err);
+          }
+        });
+      } else {
+        message.reply(':warning: Veuillez entrer un compte \n - Usage : !usertweet {compte} {option : **nombre(default: 1, max: 10)**} \n - Exemple: !usertweet FreeMemesKids 4)');
+      }
+  };
+
+  static help() {
+    return {
+      mod: "twitter",
+      corps: "**Afficher les derniers tweets d'un utilisateur :**"
+      + " \n ***!usertweet*** *{compte} {option : nombre(default: 1, max: 10)}*"
+      + "\n Exemple : !usertweet FreeMemesKids 4 \n \n",
+    };
+  };
+}
